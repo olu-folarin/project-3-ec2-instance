30d39657bb89346e897fa1eb598b5442e2dfe056
olufolarin <folami25@gmail.com>
2022-08-04 10:48:04 +0100

    setting up the ingress and egress

--- resources.tf
+++ resources.tf
@@ -1,1 +1,26 @@
 # create a security group
+resource "aws_security_group" "http_server_secg" {
+  name   = "http_server_secg"
+  vpc_id = "075343a660b9eb15c"
+
+  #   ingress specifies where traffic is allowed from
+  ingress  {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress  {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #   where the http server can communicate with
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+  }
+}
